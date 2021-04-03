ece0cfc527f3dbf063d6f55b74c2f52ee954db8f
leo-allves <leonardoalves1992@outlook.com>
2021-04-03 18:13:05 -0300

    implementação da lógica
    Foi acrescentado a impressão do resultado final no quadro no layout

--- src/js/app.js
+++ src/js/app.js
@@ -8,30 +8,39 @@ var tentativas = 3
 while (tentativas > 0){
   var chute = parseInt(prompt("Digite um número entre 0 e 10"))
 
+  
   if (numeroSecreto == chute){
-    alert("Prabéns você acertou de primeira, por isso ganhou 100 pontos!")
+    document.getElementById("quadro-reposta-id").innerHTML = "<h2>Prabéns você acertou de primeira, por isso ganhou 100 pontos!</h2>";
     //validando se acertar precisa sair //comando se acertar sair
     break //ou //tentativas = tentativas -3
 
   } else if (chute > numeroSecreto){
+    //document.getElementById("quadro-reposta-id").innerHTML = "<h2>O número secreto é menor</h2>";
     alert("O número secreto é menor")
     tentativas = tentativas -1
 
     //validando com mensagem de quantas tentativas resta
     if (tentativas == 2){
+      //document.getElementById("quadro-reposta-id").innerHTML = "<h2>Você tem mais 2 chances</h2>";
+      //document.write("<h2>Você tem mais 2 chances</h2>")
       alert("Você tem mais 2 chances")
     } else if (tentativas == 1){
+      //document.getElementById("quadro-reposta-id").innerHTML = "<h2>Você tem mais 1 chancess</h2>";
       alert("Você tem mais 1 chances")
     }
 
-  }  else if (chute < numeroSecreto){
+  } else if (chute < numeroSecreto){
+    //document.getElementById("quadro-reposta-id").innerHTML = "<h2>O número secreto é maior</h2>";
     alert("O número secreto é maior")
-    tentativas = tentativas -1
+    tentativas = tentativas -1  
 
     //validando com mensagem de quantas tentativas resta
     if (tentativas == 2){
+      //document.getElementById("quadro-reposta-id").innerHTML = "<h2>Você tem mais 2 chances</h2>";
+      //document.write("<h2>Você tem mais 2 chances</h2>")
       alert("Você tem mais 2 chances")
     } else if (tentativas == 1){
+      //document.getElementById("quadro-reposta-id").innerHTML = "<h2>Você tem mais 1 chancess</h2>";
       alert("Você tem mais 1 chances")
     }
 
@@ -39,5 +48,5 @@ while (tentativas > 0){
 }
 //condição de verificação para saber quando acaba as tentativas
 if (chute != numeroSecreto){
-  alert("Suas tentativas acabaram . O número secreto era " + numeroSecreto)
-}
+  document.getElementById("quadro-reposta-id").innerHTML = "<h2>Suas tentativas acabaram . O número secreto era " + numeroSecreto+"</h2>";
+} 
