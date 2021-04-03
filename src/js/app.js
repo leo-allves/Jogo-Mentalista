3edec208405b113b02d530ab4f2020d3bbfc722f
leo-allves <leonardoalves1992@outlook.com>
2021-04-03 15:55:37 -0300

    implementando o JS
    *sortei de número secreto
    *controlar se vai ser de 10 a 20 ....
    *validar quantas chances o jogador tem após a proxima jogada
    *laço para dar 3 tentativas após uma entrar na segunda e assim ate finalizar
    *validar se acertou, sair fora do laço e finalizar o jogo
    *mostrar mensagem do número secreto após esgotar as tentativas

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,43 @@
 //pessoa vai dar um chute no prompt
 //var numeroSecreto = parseInt(Math.random() * 10)  para de 1 a 9
-var numeroSecreto = parseInt(Math.random() * (40 - 30) + 30) 
+var numeroSecreto = parseInt(Math.random() * (10 - 0) + 0) 
 //alert(numeroSecreto) só verificando se tras um numero int
 var tentativas = 3
 
 //LOOP criando Laço WHILE(ENQUANTO)
 while (tentativas > 0){
-    var chute = parseInt(prompt("Digite um número entre 30 e 40"))
+  var chute = parseInt(prompt("Digite um número entre 0 e 10"))
 
-    if (numeroSecreto == chute){
-      alert("Acertou")
-      //validando se acertar precisa sair
-      break 
-      //ou
-      //tentativas = tentativas -3
-      //comando se acertar sair
-    } else if (chute > numeroSecreto){
-      alert("O número secreto é menor")
-      tentativas = tentativas -1
-    }  else if (chute < numeroSecreto){
-      alert("O número secreto é maior")
-      tentativas = tentativas -1
-    } 
+  if (numeroSecreto == chute){
+    alert("Prabéns você acertou de primeira, por isso ganhou 100 pontos!")
+    //validando se acertar precisa sair //comando se acertar sair
+    break //ou //tentativas = tentativas -3
+
+  } else if (chute > numeroSecreto){
+    alert("O número secreto é menor")
+    tentativas = tentativas -1
+
+    //validando com mensagem de quantas tentativas resta
+    if (tentativas == 2){
+      alert("Você tem mais 2 chances")
+    } else if (tentativas == 1){
+      alert("Você tem mais 1 chances")
+    }
+
+  }  else if (chute < numeroSecreto){
+    alert("O número secreto é maior")
+    tentativas = tentativas -1
+
+    //validando com mensagem de quantas tentativas resta
+    if (tentativas == 2){
+      alert("Você tem mais 2 chances")
+    } else if (tentativas == 1){
+      alert("Você tem mais 1 chances")
+    }
+
+  }
 }
 //condição de verificação para saber quando acaba as tentativas
 if (chute != numeroSecreto){
-    alert("Suas tentativas acabaram . O número secreto era " + numeroSecreto)
+  alert("Suas tentativas acabaram . O número secreto era " + numeroSecreto)
 }
