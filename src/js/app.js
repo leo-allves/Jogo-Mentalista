decdfe5632679630d34e2bc256f01939d30a8b9e
leo-allves <leonardoalves1992@outlook.com>
2021-04-03 15:08:10 -0300

    validação de sorteio
    passando parametro no math.random para validar no jugo no sorteio de um número x até y

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,19 @@
 //pessoa vai dar um chute no prompt
-var numeroSecreto = parseInt(Math.random() * 10)
+//var numeroSecreto = parseInt(Math.random() * 10)  para de 1 a 9
+var numeroSecreto = parseInt(Math.random() * (40 - 30) + 30) 
 //alert(numeroSecreto) só verificando se tras um numero int
 var tentativas = 3
 
 //LOOP criando Laço WHILE(ENQUANTO)
 while (tentativas > 0){
-    var chute = parseInt(prompt("Digite um número entre 0 e 10"))
+    var chute = parseInt(prompt("Digite um número entre 30 e 40"))
 
     if (numeroSecreto == chute){
       alert("Acertou")
       //validando se acertar precisa sair
       break 
+      //ou
+      //tentativas = tentativas -3
       //comando se acertar sair
     } else if (chute > numeroSecreto){
       alert("O número secreto é menor")
